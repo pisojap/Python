425df6e4921aedd673a13ab9984d9d9adef16704
peter.pisoja@gmail.com <peter.pisoja@gmail.com>
2018-03-20 16:52:55 +0100

    DB

--- DBApp/DBApp/DBApp.py
+++ DBApp/DBApp/DBApp.py
@@ -1,1 +1,28 @@
+import sqlite3
 
+conn=sqlite3.connect("lite.db")
+cur=conn.cursor()
+conn.commit()
+conn.close()
+
+def createTable():
+    with sqlite3.connect("lite.db") as conn:
+        cur=conn.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
+
+def insert(item, quantity, price):
+    with sqlite3.connect("lite.db") as conn:
+        cur=conn.cursor()
+        cur.execute("INSERT INTO store VALUES (?, ?, ?)", (item, quantity, price))
+
+def view():
+    with sqlite3.connect("lite.db") as conn:
+        cur=conn.cursor()
+        cur.execute("SELECT * FROM store")
+        rows=cur.fetchall()
+        return rows
+
+
+createTable()
+insert('Water', 10, 12.10)
+print(view())
