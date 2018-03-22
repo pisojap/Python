fe3e3b019aa6d9310b690f21bea5405995a9ceae
peter.pisoja@gmail.com <peter.pisoja@gmail.com>
2018-03-22 14:34:49 +0100

    003

--- BankAccount/BankAccount/BankAccount.py
+++ BankAccount/BankAccount/BankAccount.py
@@ -1,1 +1,11 @@
+class Account:
 
+    def __init__(self, filepath):
+        with open(filepath, 'r+') as file:
+            self.balance = int(file.read())
+
+    def getBalance(self):
+        return self.balance
+
+account = Account("balance.txt")
+print(account.getBalance())
